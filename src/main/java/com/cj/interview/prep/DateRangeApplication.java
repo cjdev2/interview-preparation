16272e3f345a5c8e61924ad63eff44450265e8f8
SeanShubin <sshubin@cj.com>
2015-02-23 16:45:16 -0800

    Added tests for date range sample

--- src/main/java/com/cj/interview/prep/DateRangeApplication.java
+++ src/main/java/com/cj/interview/prep/DateRangeApplication.java
@@ -1,10 +1,13 @@
 package com.cj.interview.prep;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 /*
 
@@ -12,44 +15,20 @@ Goal:
 * Demonstrate knowledge of refactoring, architecture, design patterns, object oriented concepts
 
 Requirements:
-* Apply the appropriate principles to this code until there is no duplication (including structural)
-* No need to bother with the database, it is not the focus of this exercise
+* Try to refactor away the need for the duplicate switch statements
+* Feel free to add new classes and/or enums
+* You should not need to modify the tests
 
-For More Information:
+If you need inspiration:
 * See page 255 of "Refactoring: Improving the Design of Existing Code", by Martin Fowler
 
 */
 public class DateRangeApplication {
     public static void main(String[] args) throws IOException {
-        new DateRangeApplication().run();
-    }
-
-    private void run() throws IOException {
-        displayChoices();
-        int userChoice = getUserChoice();
-        generateReport(userChoice);
-    }
-
-    private void displayChoices() {
-        System.out.println("Choose the date range to report on: ");
-        for (int i = 0; i < DateRangeUtil.timeRangeCount; i++) {
-            System.out.println(String.format("(%d) %s", i, DateRangeUtil.getDisplayString(i)));
-        }
-    }
-
-    private int getUserChoice() throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        return Integer.parseInt(in.readLine());
+        new DateRangeApplication().testSuite();
     }
 
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
-    static class DatabaseUtil {
-        static void runSql(String sql) {
-            //we are not worried about hooking up the database yet
-            System.out.println("sending to database: " + sql);
-        }
-    }
 
     static class DateRangeUtil {
         static int timeRangeCount = 6;
@@ -74,57 +53,118 @@ public class DateRangeApplication {
         }
     }
 
-    private void generateReport(int userSelection) {
+    private String generateQuery(int userSelection, ZonedDateTime now) {
         switch (userSelection) {
             case 0:
-                DatabaseUtil.runSql("select * from interesting_data where last_updated > sysdate() - 7");
-                break;
+                return "select * from interesting_data where last_updated > sysdate() - 7";
             case 1:
-                DatabaseUtil.runSql("select * from interesting_data where last_updated > sysdate() - 30");
-                break;
+                return "select * from interesting_data where last_updated > sysdate() - 30";
             case 2: {
-                LocalDate now = LocalDate.now();
                 int month = now.getMonthValue();
                 int quarter = (month + 2) / 3;
                 int monthAtBeginningOfQuarter = quarter * 3 - 2;
-                LocalDate endQuarter = now.withDayOfMonth(1).withMonth(monthAtBeginningOfQuarter);
-                LocalDate beginQuarter = endQuarter.minusMonths(3);
-                DatabaseUtil.runSql(String.format(
+                ZonedDateTime endQuarter = now.withDayOfMonth(1).withMonth(monthAtBeginningOfQuarter);
+                ZonedDateTime beginQuarter = endQuarter.minusMonths(3);
+                return String.format(
                         "select * from interesting_data where last_updated between '%s' and '%s'",
                         formatter.format(beginQuarter),
-                        formatter.format(endQuarter)));
+                        formatter.format(endQuarter));
             }
-            break;
             case 3: {
-                LocalDate today = LocalDate.now();
-                int year = today.getYear();
-                LocalDate lastYear = today.withYear(year - 1).withDayOfYear(1);
-                LocalDate thisYear = today.withYear(year).withDayOfYear(1);
-                DatabaseUtil.runSql(String.format(
+                int year = now.getYear();
+                ZonedDateTime lastYear = now.withYear(year - 1).withDayOfYear(1);
+                ZonedDateTime thisYear = now.withYear(year).withDayOfYear(1);
+                return String.format(
                         "select * from interesting_data where last_updated between '%s' and '%s'",
                         formatter.format(lastYear),
-                        formatter.format(thisYear)));
+                        formatter.format(thisYear));
             }
-            break;
             case 4: {
-                LocalDate today = LocalDate.now();
-                LocalDate beginningOfYear = today.withDayOfYear(1);
-                DatabaseUtil.runSql(String.format(
+                ZonedDateTime beginningOfYear = now.withDayOfYear(1);
+                return String.format(
                         "select * from interesting_data where last_updated > '%s'",
-                        formatter.format(beginningOfYear)));
+                        formatter.format(beginningOfYear));
             }
-            break;
             case 5: {
-                LocalDate today = LocalDate.now();
-                int month = today.getMonthValue();
+                int month = now.getMonthValue();
                 int quarter = (month + 2) / 3;
                 int monthAtBeginningOfQuarter = quarter * 3 - 2;
-                LocalDate beginQuarter = today.withDayOfMonth(1).withMonth(monthAtBeginningOfQuarter);
-                DatabaseUtil.runSql(String.format(
+                ZonedDateTime beginQuarter = now.withDayOfMonth(1).withMonth(monthAtBeginningOfQuarter);
+                return String.format(
                         "select * from interesting_data where last_updated > '%s'",
-                        formatter.format(beginQuarter)));
+                        formatter.format(beginQuarter));
             }
-            break;
+            default:
+                throw new RuntimeException(String.format("Invalid id %d", userSelection));
+        }
+    }
+
+    private String queryForOption(String option, ZonedDateTime now) {
+        int optionId = -1;
+        for (int i = 0; i < DateRangeUtil.timeRangeCount; i++) {
+            if (DateRangeUtil.getDisplayString(i).equals(option)) {
+                optionId = i;
+                break;
+            }
+        }
+        String query = generateQuery(optionId, now);
+        return query;
+    }
+
+    private List<String> getDateRangesForUser() {
+        List<String> dateRanges = new ArrayList<>();
+        for (int i = 0; i < DateRangeUtil.timeRangeCount; i++) {
+            String dateRange = DateRangeUtil.getDisplayString(i);
+            dateRanges.add(dateRange);
+        }
+        return dateRanges;
+    }
+
+    private void testSuite() {
+        testOptionsDisplayedToUser();
+        testGeneratedDatabaseQueries();
+
+        System.out.println("ALL TESTS PASSED!");
+    }
+
+    private void testOptionsDisplayedToUser() {
+        List<String> optionsVisibleToUser = getDateRangesForUser();
+
+        intsEqual(optionsVisibleToUser.size(), 6);
+
+        stringsEqual(optionsVisibleToUser.get(0), "Last 7 Days");
+        stringsEqual(optionsVisibleToUser.get(1), "Last 30 Days");
+        stringsEqual(optionsVisibleToUser.get(2), "Last Quarter");
+        stringsEqual(optionsVisibleToUser.get(3), "Last Year");
+        stringsEqual(optionsVisibleToUser.get(4), "Year to Date");
+        stringsEqual(optionsVisibleToUser.get(5), "Quarter to Date");
+    }
+
+    private void testGeneratedDatabaseQueries() {
+        LocalDate date = LocalDate.of(2014, 8, 23);
+        LocalTime time = LocalTime.of(3, 15, 20);
+        ZoneId zone = ZoneId.of("UTC");
+        ZonedDateTime now = ZonedDateTime.of(date, time, zone);
+
+        stringsEqual(queryForOption("Last 7 Days", now), "select * from interesting_data where last_updated > sysdate() - 7");
+        stringsEqual(queryForOption("Last 30 Days", now), "select * from interesting_data where last_updated > sysdate() - 30");
+        stringsEqual(queryForOption("Last Quarter", now), "select * from interesting_data where last_updated between '2014-04-01' and '2014-07-01'");
+        stringsEqual(queryForOption("Last Year", now), "select * from interesting_data where last_updated between '2013-01-01' and '2014-01-01'");
+        stringsEqual(queryForOption("Year to Date", now), "select * from interesting_data where last_updated > '2014-01-01'");
+        stringsEqual(queryForOption("Quarter to Date", now), "select * from interesting_data where last_updated > '2014-07-01'");
+    }
+
+    private void intsEqual(int actual, int expected) {
+        if (actual != expected) {
+            String message = String.format("FAILURE: expected '%d', but got '%d'", expected, actual);
+            throw new RuntimeException(message);
+        }
+    }
+
+    private void stringsEqual(String actual, String expected) {
+        if (!actual.equals(expected)) {
+            String message = String.format("FAILURE: expected '%s', but got '%s'", expected, actual);
+            throw new RuntimeException(message);
         }
     }
 }
